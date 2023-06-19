ab2f83805aedcb2223f1d1cf95a6bace9255b07a
Enoisong <enobongitiaba988@gmail.com>
2023-06-19 08:24:42 -0700

    fix linters

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './index.css';
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import Navbar from "./components/Navbar";
+import Navbar from './components/Navbar';
 import Books from './components/Books';
 import Categories from './components/Categories';
 
